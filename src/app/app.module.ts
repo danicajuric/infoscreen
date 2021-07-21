67639d39c0b903888cb52d4c024ec9355a3304a5
unknown <danica.juric@fsre.sum.ba>
2021-07-21 12:35:48 +0200

    Created dynamic background component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { DynamicBackgroundComponent } from './dynamic-background/dynamic-background.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    DynamicBackgroundComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/dynamic-background/dynamic-background.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dynamic-background',
+  templateUrl: './dynamic-background.component.html',
+  styleUrls: ['./dynamic-background.component.css']
+})
+export class DynamicBackgroundComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

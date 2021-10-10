14194d607c0b3bc0d370444d31d1035e87c80c41
unknown <danica.juric@fsre.sum.ba>
2021-10-10 09:35:19 +0200

    changed color

--- src/app/testcalendar/testcalendar.component.spec.ts
+++ src/app/testcalendar/testcalendar.component.spec.ts
@@ -2,10 +2,11 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TestcalendarComponent } from './testcalendar.component';
 
+
 describe('TestcalendarComponent', () => {
   let component: TestcalendarComponent;
   let fixture: ComponentFixture<TestcalendarComponent>;
-
+  
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ TestcalendarComponent ]
